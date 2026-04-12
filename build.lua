ad253e86dd93655f3515904b7090e09867f4e4ed
Mingyu Hsia <myhsia@outlook.com>
2026-04-12 20:48:55 +0800

    first commit

---
+++ build.lua
@@ -0,0 +1,24 @@
+--[==========================================[--
+    L3BUILD FILE FOR "CHEM5024@Spectroscopy"
+--]==========================================]--
+
+module           = "CHEM5024@Spectroscopy"
+version          = "2026-04"
+docsuppdirs      = {"context", "media"}
+unpacksuppfiles  = {"*.bib"}
+textfiles        = {"*.md", "LICENSE", "*.lua"}
+typesetexe       = "latexmk"
+typesetfiles     = {module .. ".tex"}
+typesetopts      = "-pdf -interaction=nonstopmode"
+
+function docinit_hook()
+  for _,glob in pairs(docsuppdirs) do
+    run(currentdir, "cp -r " .. glob .. " " .. typesetdir)
+  end
+  return 0
+end
+function tex(file,dir,cmd)
+  dir = dir or "."
+  cmd = cmd or typesetexe .. " " .. typesetopts
+  return run(dir, cmd .. file)
+end
